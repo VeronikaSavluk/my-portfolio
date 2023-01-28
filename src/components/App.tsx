a7d7b85d66bf4d9628bad625c31593871ef46d6b
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-01-28 21:57:42 +0200

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import About from './About';
 import logo from '../image/logo.png';
 import '../App.css';
 
@@ -10,6 +11,7 @@ function App() {
         <h1>Veronika Savluk</h1>
         <p>Full Stack Developer</p>
       </header>
+      <About/>
     </div>
   );
 }
