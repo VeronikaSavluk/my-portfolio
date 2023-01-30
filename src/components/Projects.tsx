7bba2d7c3042f439fc10c5132373ef3a4e26a210
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-01-30 20:55:37 +0200

    Update Projects.tsx

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -10,17 +10,20 @@ import projects from '../projects.json';
 
 function projectsByType(type: String) {
 	const typeProjects = projects.filter(project => project.type === type)
-	return typeProjects.map(({name, codeURL, websiteURL, description, tools}, idx) => {
-		const libraries = Object.values(tools).join(', ');
-		return (
-		<GridItem key={idx + 1} justifySelf='start' justifyContent='flex-start'>
-			<Link href={websiteURL}>{name}</Link>
-			<Text>{description}</Text>
-			<Link textTransform='capitalize' href={codeURL}>{libraries}</Link>
-		</GridItem>
-		)}
-		)
-}
+	return (
+		<Grid>
+			{typeProjects.map(({name, codeURL, websiteURL, description, tools}, idx) => {
+			const libraries = Object.values(tools).join(', ');
+			return (
+				<GridItem key={idx + 1} justifySelf='start' justifyContent='flex-start'>
+					<Link href={websiteURL}>{name}</Link>
+					<Text>{description}</Text>
+					<Link textTransform='capitalize' href={codeURL}>{libraries}</Link>
+				</GridItem>
+				)}
+			)}
+		</Grid>
+	)}
 
 function Projects (){
 return (
