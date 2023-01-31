74abe3293722d490693ea066b13249fdcc18b60b
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-01-31 20:28:04 +0200

    Create AppBar.tsx

---
+++ src/components/AppBar.tsx
@@ -0,0 +1,30 @@
+import { NavLink } from 'react-router-dom';
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink
+} from '@chakra-ui/react';
+
+function AppBar () {
+	return (
+		<Breadcrumb fontWeight='bold' fontSize='xl' separator=' '>
+		<BreadcrumbItem>
+				<BreadcrumbLink as={NavLink} to='/' isCurrentPage>
+				About
+				</BreadcrumbLink>
+		</BreadcrumbItem>
+		<BreadcrumbItem>
+				<BreadcrumbLink as={NavLink} to='/projects'>
+				Projects
+				</BreadcrumbLink>
+		</BreadcrumbItem>
+		<BreadcrumbItem>
+				<BreadcrumbLink as={NavLink} to='/skills'>
+				Skills
+				</BreadcrumbLink>
+		</BreadcrumbItem>
+		</Breadcrumb>
+	)
+};
+
+export default AppBar;
