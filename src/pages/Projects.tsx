db0de8f7dbc894ab5ec9dc20fdc8f4f2d01ab91d
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-02-27 02:20:42 +0200

    Update Projects.tsx

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -2,42 +2,51 @@ import {
 	Text,
 	Link,
 	Heading,
+	OrderedList,
+	ListItem,
 	Grid,
-	GridItem, 
-	Flex} from '@chakra-ui/react';
+	Flex
+} from '@chakra-ui/react';
 	
 import projects from '../projects.json';
 
 function projectsByType(type: String) {
 	const typeProjects = projects.filter(project => project.type === type)
 	return (
-		<Grid>
+		<OrderedList>
 			{typeProjects.map(({name, codeURL, websiteURL, description, tools}, idx) => {
 			const libraries = Object.values(tools).join(', ');
 			return (
-				<GridItem key={idx + 1} justifySelf='start' justifyContent='flex-start'>
-					<Link href={websiteURL}>{name}</Link>
+				<ListItem key={idx + 1} justifySelf='start' justifyContent='flex-start'>
+					<Link href={websiteURL} target="_blank">{name}</Link>
+					<Link href={codeURL} target="_blank"> [code resource]:</Link>
 					<Text>{description}</Text>
-					<Link textTransform='capitalize' href={codeURL}>{libraries}</Link>
-				</GridItem>
+					<Text>Technologies: {libraries}</Text>
+				</ListItem>
 				)}
 			)}
-		</Grid>
+		</OrderedList>
 	)}
 
 function Projects (){
 return (
+	<>
+	<Heading textTransform='capitalize'>Projects</Heading>
 	<Flex justifyItems='start'>
-		<Heading textTransform='capitalize'>Projects</Heading>
+		<Flex flexDirection="column">
 		<Heading textTransform='capitalize'>team</Heading>
-		<Grid justifyItems='start'>
+		<Grid justifyItems='start' width="48vw">
 			{projectsByType('team')}
 		</Grid>
+		</Flex>
+		<Flex flexDirection="column">
 		<Heading textTransform='capitalize'>individual</Heading>
-		<Grid justifyItems='start'>
+		<Grid justifyItems='start' width="48vw">
 			{projectsByType('individual')}
 		</Grid>
+		</Flex>
 	</Flex>
+	</>
 	)
 };
 
