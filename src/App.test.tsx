f174a79e77d9961b6d8d9f29e34ff05dce1dec1a
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-03-13 00:32:46 +0200

    Delete App.test.tsx

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './components/App';
-
-test('renders position', () => {
-  render(<App />);
-  const positionElement = screen.getByText(/Full Stack Developer/i);
-  expect(positionElement).toBeInTheDocument();
-});
