c3d188a8102911d86a7dabf013d357f6f8620386
Veronika Savluk <98710326+VeronikaSavluk@users.noreply.github.com>
2023-03-18 02:40:48 +0200

    corrected breakpoints

--- src/theme/components/Container.ts
+++ src/theme/components/Container.ts
@@ -3,7 +3,7 @@ import { defineStyleConfig } from "@chakra-ui/react";
 export const Container = defineStyleConfig({
   baseStyle: {
     minW: '320px',
-    maxW: '900px',
+    maxW: '1280px',
     px: '20px',
     pt: '20px',
   },
--- src/theme/foundations/breakpoints.ts
+++ src/theme/foundations/breakpoints.ts
@@ -2,6 +2,6 @@ export const breakpoints = {
 	sm: '320px',
 	md: '768px',
 	lg: '960px',
-	xl: '1200px',
+	xl: '1280px',
 	'2xl': '1680px',
 };
